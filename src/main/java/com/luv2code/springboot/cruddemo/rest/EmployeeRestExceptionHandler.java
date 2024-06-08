4b524f408d9e7319080ed23b1dec4c830992d751
PHANINDRA SIDHANTHI <phani.sidhanthi03@gmail.com>
2024-06-07 20:04:20 -0500

    Basic Spring Boot CRUD Application

---
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.luv2code.springboot.cruddemo.rest;
+
+import com.luv2code.springboot.cruddemo.entity.EmployeeErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class EmployeeRestExceptionHandler {
+
+    //add Exception handling methods
+    @ExceptionHandler
+    public ResponseEntity<EmployeeErrorResponse> handleException(EmployeeNotFoundException exc) {
+        EmployeeErrorResponse error = new EmployeeErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeErrorResponse> handleException(Exception exe) {
+        EmployeeErrorResponse error = new EmployeeErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exe.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+}
